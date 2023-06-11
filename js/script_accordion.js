ba706c33ef38f243aa015d579673757290f2f312
ShunMare <ultrasoul0714@gmail.com>
2023-06-11 10:11:18 +0900

    Modify work layout

--- js/script_accordion.js
+++ js/script_accordion.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".accordion-wrap").first().children().eq(1).show();
-  $(".accordion-wrap").first().find(".accordion-header").addClass("accordion-gold");
+  $(".accordion-wrap").first().find(".accordion-header").addClass("accordion-color");
   $(".accordion-wrap").first().find(".fa").addClass("rotate-fa");
 
   $(".accordion-wrap").on("click", function(){
@@ -9,9 +9,9 @@ $(document).ready(function() {
     }
     $(this).children().eq(1).slideToggle(300);  
     $(this).children().eq(0).toggleClass("accordion-no-bar");
-    $(this).siblings().find(".accordion-header").removeClass("accordion-gold");
+    $(this).siblings().find(".accordion-header").removeClass("accordion-color");
     $(this).siblings().find(".accordion-header i").removeClass("rotate-fa");
-    $(this).find(".accordion-header").toggleClass("accordion-gold");
+    $(this).find(".accordion-header").toggleClass("accordion-color");
     $(this).find(".fa").toggleClass("rotate-fa");
     $(".accordion-wrap .accordion-text").not($(this).children().eq(1)).slideUp(300);
   });
